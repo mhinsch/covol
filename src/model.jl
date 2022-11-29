593f0d65db6d183d165688da3f84aee269b4e73d
Martin Hinsch <hinsch.martin@gmail.com>
2022-11-29 17:04:42 +0000

    refactor cont

--- src/model.jl
+++ src/model.jl
@@ -53,6 +53,41 @@ function arrive!(agent, world, pars)
 end
 
 
+# done globally for now
+# TODO take into account viral load
+function infection!(place, world, pars)
+    inf = Agent[]; susc = Agent[]
+    for a in place.present
+        if infectious(a)
+            push!(inf, a)
+        elseif susceptible(a)
+            push!(susc, a)
+        end
+    end
+
+    if isempty(inf) || isempty(susc)
+        return
+    end
+
+    n_pairs = length(inf) * length(susc)
+
+    # determine number of encounters
+    if n_pairs > 15 && pars.p_encounter < 0.1
+        # good approximation for these numbers
+        n_enc = rand(Poisson(n_pairs * pars.p_encounter))
+    else
+        n_enc = 0
+        for i in 1:n_pairs
+            if rand() < pars.p_encounter
+                n_enc += 1
+            end
+        end
+    end
+
+    for e in n_enc
+        encounter!(rand(inf), rand(susc), world.ief, pars)
+    end
+end
 
 # only evolution if IEF for now
 function virus!(virus, ief, pars)
--- src/model_infection.jl
+++ src/model_infection.jl
@@ -6,7 +6,7 @@ function inf_rate(infectee, virus, pars)
 end
 
 
-function infect!(agent, virus, world, pars)
+function infect!(agent, virus, ief, pars)
     agent.immune.status = IStatus.infected
     agent.virus.age = 0
     agent.virus.ief_0 = transmitted_ief(virus.age, world.ief, pars) * virus.ief_0
@@ -15,47 +15,12 @@ end
 
 
 # very simplistic model for now
-function encounter!(inf, susc, place, world, pars)
+function encounter!(inf, susc, ief, pars)
     p_inf = inf_rate(susc, inf.virus, pars)
 
     if rand() < p_inf
-        infect!(susc, inf.virus, world, pars)
+        infect!(susc, inf.virus, ief, pars)
     end
 end
 
 
-# done globally for now
-# TODO take into account viral load
-function infection!(place, world, pars)
-    inf = Agent[]; susc = Agent[]
-    for a in place.present
-        if infectious(a)
-            push!(inf, a)
-        elseif susceptible(a)
-            push!(susc, a)
-        end
-    end
-
-    if isempty(inf) || isempty(susc)
-        return
-    end
-
-    n_pairs = length(inf) * length(susc)
-
-    # determine number of encounters
-    if n_pairs > 15 && pars.p_encounter < 0.1
-        # good approximation for these numbers
-        n_enc = rand(Poisson(n_pairs * pars.p_encounter))
-    else
-        n_enc = 0
-        for i in 1:n_pairs
-            if rand() < pars.p_encounter
-                n_enc += 1
-            end
-        end
-    end
-
-    for e in n_enc
-        encounter!(rand(inf), rand(susc), place, world, pars)
-    end
-end
