d054c81aed86a46cb94feffc36d6626db0869e7a
Martin Hinsch <hinsch.martin@gmail.com>
2023-03-11 16:32:14 +0000

    measure gene distances

--- agab_sample.jl
+++ agab_sample.jl
@@ -1,6 +1,13 @@
+function gene_dist(ga, gb)
+    d = 0.0
+    for (a, b) in zip(ga, gb)
+        d += abs(a-b)
+    end
+    
+    d/length(ga)
+end
 
-
-function sample_virus_genes(pop)
+function sample_virus_genes(pop, compare = nothing)
     cdict = Dict{typeof(pop[1].virus.antigens), Int}() 
     for a in pop
         if ! isempty(a.virus.antigens)
@@ -9,6 +16,21 @@ function sample_virus_genes(pop)
         end
     end
     
-    sort!(collect(cdict), by = x->x.second)
+    genes = sort!(collect(cdict), by = x->x.second)
+    
+    if isempty(genes)
+        return genes, Float64[], Float64[]
+    end
+    
+    best = genes[end][1]
+    
+    dist1 = [ gene_dist(best, g[1]) for g in genes]
+    if compare != nothing
+        dist2 = [ gene_dist(compare, g[1]) for g in genes]
+    else
+        dist2 = Float64[]
+    end
+    
+    genes, dist1, dist2
 end
         
